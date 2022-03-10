aa7ff1bbf5147ffe965ce509e7866a7ba005675d
neil514 <neil.rego@outlook.com>
2022-03-10 16:13:19 -0500

    initial commit

---
+++ src/app.js
@@ -0,0 +1,109 @@
+// nodemon ./src/app.js -e js,hbs to run nodemon for this app
+import { fileURLToPath } from "url";
+import path from "path";
+import express from "express";
+import hbs from "hbs";
+import { geocode } from "./utils/geocode.js";
+import { forecast } from "./utils/forecast.js";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// Define paths for Express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+// Setup Handlebard engine and Views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup Static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Neil Rego",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Neil Rego",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    name: "Neil Rego",
+    helpText: "This is a message that contains help.",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  if (!req.query.address) {
+    return res.send({
+      error: "You must provide and address.",
+    });
+  }
+
+  geocode(
+    req.query.address,
+    (error, { latitude, longitude, location } = {}) => {
+      if (error) {
+        return res.send({ error });
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+        if (error) {
+          return res.send({ error });
+        }
+
+        res.send({
+          forecast: forecastData,
+          location: location,
+          address: req.query.address,
+        });
+      });
+    }
+  );
+});
+
+app.get("/products", (req, res) => {
+  if (!req.query.search) {
+    return res.send({
+      error: "You must provide a search term.",
+    });
+  }
+
+  console.log(req.query.search);
+  res.send({
+    products: [],
+  });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404 Not Found",
+    name: "Neil Rego",
+    errorMessage: "Help article not found.",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404 Not Found",
+    name: "Neil Rego",
+    errorMessage: "Page not found.",
+  });
+});
+
+app.listen(3000, () => {
+  console.log("Server is ip on port 3000.");
+});
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+import request from "postman-request";
+
+export const forecast = (latitude, longitude, callback) => {
+  const url = `http://api.weatherstack.com/current?access_key=923cffb451a06cb1032a15562250ef92&query=${latitude},${longitude}`;
+
+  request({ url, json: true }, (error, { body } = {}) => {
+    if (error) {
+      callback("Unable to connect to the weather service.", undefined);
+    } else if (body.error) {
+      callback("Unbale to find location.", undefined);
+    } else {
+      callback(
+        undefined,
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
+      );
+    }
+  });
+};
